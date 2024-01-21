d369a22bc255f690a23a5a5f41e6bffea69e4646
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-01-21 15:01:30 +0530

    project completed

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   // Result to be displayed
   let result = "";
   let display = document.getElementById("display");
@@ -11,14 +10,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let divide = document.getElementById("divide");
   let sqrt = document.getElementById("sqrt");
   let clr = document.getElementById("clr");
+  let decimal = document.getElementById("decimal");
+  let eql = document.getElementById("eql");
 
+  // Event Listener
   ac.addEventListener("click", clearDisplay);
   plus.addEventListener("click", sum);
   minus.addEventListener("click", subtract);
   multiply.addEventListener("click", multiple);
   divide.addEventListener("click", division);
   clr.addEventListener("click", backspace);
-  
+  sqrt.addEventListener("click", root);
+  decimal.addEventListener("click", point);
+  eql.addEventListener("click", () => {
+    display.value = calculateResult(result);
+    result = display.value;
+  });
+
+  // TO add plus sign
+
   function sum() {
     if (result.length != 0) {
       if (result[result.length - 1] != "+") {
@@ -35,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
+  // TO add minus sign
   function subtract() {
     if (result.length != 0) {
       if (result[result.length - 1] != "-") {
@@ -50,6 +61,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     return;
   }
+
+  // TO add multiply sign
 
   function multiple() {
     if (result.length != 0) {
@@ -67,6 +80,8 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
+  // TO add division sign
+
   function division() {
     if (result.length != 0) {
       if (result[result.length - 1] != "/") {
@@ -83,18 +98,182 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
-function backspace(){
-    result = result.slice(0,-1);
-    display.value = result
-}
+  // To move one step back
+  function backspace() {
+    result = result.slice(0, -1);
+    display.value = result;
+  }
 
+  // To add sqrt
+  function root() {
+    if (result.length == 0) {
+      result += "sqrt";
+      display.value = result;
+    } else {
+      window.alert("in-correct operation");
+    }
+  }
 
-  function calculateResult() {}
+  // TO add decimal
+
+  function point() {
+    if (result.slice(-1) != ".") {
+      result += ".";
+      display.value = result;
+    }
+  }
+
+  // It trigger after clicking on equal to button
+
+  function calculateResult(str) {
+    console.log(str);
+    let arr = [];
+    str = str.toString();
+    let flag = true;
+    let num = 0;
+    if (str.length != 0) {
+      if (str[0] == "s") {
+        flag = false;
+        str = str.slice(4);
+        if (
+          str.slice(-1) == "+" ||
+          str.slice(-1) == "-" ||
+          str.slice(-1) == "*" ||
+          str.slice(-1) == "/"
+        ) {
+          for (let i = 0; i < str.length - 1; i++) {
+            if (
+              str[i] == "+" ||
+              str[i] == "-" ||
+              str[i] == "*" ||
+              str[i] == "/"
+            ) {
+              arr.push(Number(num));
+              num = 0;
+              arr.push(str[i]);
+            } else if (i == str.length - 2) {
+              arr.push(Number(num + str[i]));
+              num = 0;
+            } else {
+              num += str[i];
+            }
+          }
+        } else {
+          for (let i = 0; i < str.length; i++) {
+            if (
+              str[i] == "+" ||
+              str[i] == "-" ||
+              str[i] == "*" ||
+              str[i] == "/"
+            ) {
+              arr.push(Number(num));
+              num = 0;
+              arr.push(str[i]);
+            } else if (i == str.length - 1) {
+              arr.push(Number(num + str[i]));
+              num = 0;
+            } else {
+              num += str[i];
+            }
+          }
+        }
+      } else {
+        if (
+          str.slice(-1) == "+" ||
+          str.slice(-1) == "-" ||
+          str.slice(-1) == "*" ||
+          str.slice(-1) == "/"
+        ) {
+          for (let i = 0; i < str.length - 1; i++) {
+            if (
+              str[i] == "+" ||
+              str[i] == "-" ||
+              str[i] == "*" ||
+              str[i] == "/"
+            ) {
+              arr.push(Number(num));
+              num = 0;
+              arr.push(str[i]);
+            } else if (i == str.length - 2) {
+              arr.push(Number(num + str[i]));
+              num = 0;
+            } else {
+              num += str[i];
+            }
+          }
+        } else {
+          for (let i = 0; i < str.length; i++) {
+            if (
+              str[i] == "+" ||
+              str[i] == "-" ||
+              str[i] == "*" ||
+              str[i] == "/"
+            ) {
+              arr.push(Number(num));
+              num = 0;
+              arr.push(str[i]);
+            } else if (i == str.length - 1) {
+              arr.push(Number(num + str[i]));
+              num = 0;
+            } else {
+              num += str[i];
+            }
+          }
+        }
+      }
+
+      while (arr.length != 1) {
+        if (arr.indexOf("/") != -1) {
+          while (arr.indexOf("/") != -1) {
+            // [1,2,5,7,8]
+            let index = arr.indexOf("/");
+            let final = arr[index - 1] / arr[index + 1];
+            arr.splice(index - 1, 3, final);
+          }
+        }
+        if (arr.indexOf("*") != -1) {
+          while (arr.indexOf("*") != -1) {
+            // [1,2,5,7,8]
+            let index = arr.indexOf("*");
+            let final = arr[index - 1] * arr[index + 1];
+            arr.splice(index - 1, 3, final);
+          }
+        }
+        if (arr.indexOf("+") != -1) {
+          while (arr.indexOf("+") != -1) {
+            // [1,2,5,7,8]
+            let index = arr.indexOf("+");
+            let final = arr[index - 1] + arr[index + 1];
+            arr.splice(index - 1, 3, final);
+          }
+        }
+        if (arr.indexOf("-") != -1) {
+          while (arr.indexOf("-") != -1) {
+            // [1,2,5,7,8]
+            let index = arr.indexOf("-");
+            let final = arr[index - 1] - arr[index + 1];
+            arr.splice(index - 1, 3, final);
+          }
+        }
+      }
+      if (flag) {
+        return arr[0].toFixed(2);
+      } else {
+        return Math.sqrt(arr[0]).toFixed(4);
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  // For ac button
 
   function clearDisplay() {
     result = "";
     display.value = result;
   }
+
+  // applying event listener for all number
 
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
